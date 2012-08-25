a1bac2f13622c3c53d55817e5f53fec0659d5470
Dan Gibbs <daniel.gibbs@gmail.com>
2012-08-25 21:04:43 +0100

    Initial i18n (lexicon) files

---
+++ core/components/localweather/lexicon/en/properties.inc.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Local Weather
+ *
+ * Copyright (c) 2012 Gold Coast Media <dan@goldcoastmedia.co.uk>.
+ *
+ * This file is part of Local Weather for MODx.
+ *
+ * Local Weather is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Local Weather is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Local Weather for MODx; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Default English Topic for Local Weather for MODx.
+ *
+ * @package     localweather
+ * @author      Dan Gibbs <dan@goldcoastmedia.co.uk>
+ * @copyright   Copyright (c) 2012 Gold Coast Media Ltd
+ * @subpackage  lexicon
+ * @language    en
+ */
+ 
+$_lang['cachelifetime'] = 'The amount of time to cache the weather feed.';
+$_lang['cachename']     = 'The name of the cache feed.';
+$_lang['css']           = 'A comma separated list of CSS files to insert.';
+$_lang['method']        = 'The method used to fetch the the remote feed. cURL or file_get_contents.';
+
